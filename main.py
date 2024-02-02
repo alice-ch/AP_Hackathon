843d17b347cdf17c38e36f990e448b12e2ae4e18
Ombline Brunet <bonbonblond@laposte.net>
2024-02-02 10:37:55 +0100

    Update

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import cours_pygame
+import pygame
 import random
 import numpy as np
 
 SCREEN_COLOR = (100, 100, 100)
-SCREEN_WIDTH = 200
-SCREEN_HEIGHT = 500
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
 CLOCK_FREQUENCY = 5
 
 TILES_SIZE = 20
@@ -13,4 +13,40 @@ TILES_COLOR = (50, 50, 50)
 NUMBER_OF_TILES_HEIGHT = SCREEN_HEIGHT // TILES_SIZE
 NUMBER_OF_TILES_WIDGHT = SCREEN_WIDTH // TILES_SIZE
 
+class Game():
+    def __init__(self, screen):
+        self.is_running = True
+        self.screen = screen
+    
+    def display_checkerboard(self):
+        self.screen.fill(SCREEN_COLOR)
+        k, l = int(SCREEN_HEIGHT / TILES_SIZE), int(SCREEN_WIDTH / TILES_SIZE)
+        for i in range(k):
+            for j in range(l):
+                if (i + j) % 2 == 1:
+                    rect = pygame.Rect(
+                        j * TILES_SIZE, i * TILES_SIZE, TILES_SIZE, TILES_SIZE
+                    )
+                    pygame.draw.rect(self.screen, TILES_COLOR, rect)
+    
+    def display(self):
+        self.display_checkerboard()
 
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    game = Game(screen)
+
+    while game.is_running:
+        pygame.display.set_caption("Rogue")
+        clock.tick(CLOCK_FREQUENCY)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game.is_running = False
+        game.display()
+        pygame.display.update()
+
+
+main()
