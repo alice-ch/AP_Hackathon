feb2c03d3835482c09ed580ad156f2ff885537c7
Alice C <alice.chakroun123@gmail.com>
2024-02-02 11:57:59 +0100

    objet

--- main.py
+++ main.py
@@ -31,6 +31,12 @@ CORRIDORS_LENGTHy = [0,5,0,6,5,0,0]
 MONEYx = [14,30,30,31,31]
 MONEYy = [14,21,22,21,22]
 
+POTIONx=[]
+POTIONy=[]
+
+WEAPONx=[]
+WEAPONy=[]
+
 FOODx = [10,24,39,51,15,29]
 FOODy = [2,1,7,4,15,15]
 
@@ -117,21 +123,25 @@ class Piece:
 def Object:
     def __init__(self):
         self.colors=([(173,255,47),      #potion vert 
-                      (220,20,60),       # monstre rouge
-                      (238,130,238),     # armes violet
-                      (,,),                #water
-                      (,,),                #food
-                      (255,215,0)])      # tresor jaune
-                                        
+                      (220,20,60),       #monstre rouge
+                      (238,130,238),     #armes violet
+                      (240,248,250),     #water
+                      (128,0,0),         #food
+                      (255,215,0)])      #tresor jaune
                                         
         self.shape = np.array([1])
-        self.position = [
-            0,
-            NUMBER_OF_TILES_WIDGHT // 2 - 1,
-        ]  # middle pour l'instant, à modifier
-        self.deplacement = 0 # pas de déplacement initial (prend des valeurs entre 0 et 4, 0 à l'arret, 1G, 2D, 3H, 4B)
-        self.vie = 5 # barre de vie qui est vouée à décroitre (ou augmenter)
-        self.colour = (135,206,235)
+
+
+    def display_object(self):
+        #potion vert
+    
+        #armes violet
+    
+        #water
+    
+        #food
+    
+        #tresor jaune
 
 
 def main():
