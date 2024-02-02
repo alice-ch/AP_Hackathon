d843239b61b781b98acfef7e0136d240750ec1c8
Ombline Brunet <bonbonblond@laposte.net>
2024-02-02 10:44:05 +0100

    ajout de la classe piece

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import pygame
 import random
 import numpy as np
 
-SCREEN_COLOR = (100, 100, 100)
+SCREEN_COLOR = (50, 50, 50)
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
 CLOCK_FREQUENCY = 5
 
-TILES_SIZE = 20
-TILES_COLOR = (50, 50, 50)
+TILES_SIZE = 22
+TILES_COLOR = (15, 15, 15)
 
 NUMBER_OF_TILES_HEIGHT = SCREEN_HEIGHT // TILES_SIZE
 NUMBER_OF_TILES_WIDGHT = SCREEN_WIDTH // TILES_SIZE
@@ -32,6 +32,42 @@ class Game():
     def display(self):
         self.display_checkerboard()
 
+
+class Piece:
+    def __init__(self):
+        self.shape = np.array([1])
+        self.position = [
+            0,
+            NUMBER_OF_TILES_WIDGHT // 2 - 1,
+        ]  # middle pour l'instant, à modifier
+        self.colour = (135,206,235) # il est bleu !!!
+
+    def display(self, screen):
+        x = self.position[0] * TILES_SIZE
+        y = self.position[1] * TILES_SIZE
+        rect = pygame.Rect(y, x, TILES_SIZE, TILES_SIZE)
+        pygame.draw.rect(screen,self.colour,rect)
+
+    def new_position(self, screen):
+        former_position = np.copy(self.position)
+
+        if.... gauche:
+            self.shape = [former_position[0] -1 ,former_position[1]  ]
+        elif.... droite:
+            self.shape = [former_position[0] + 1 ,former_position[1] ]
+        elif.... haut:
+            self.shape = [former_position[0] ,former_position[1] +1]
+        elif.... droit :
+            self.shape = [former_position[0] ,former_position[1] -1 ]
+
+
+        if self.has_collided(screen) : #si il rentre dans un mur, # pour l'instant true/false mais lv up possible
+            self.shape = former_position
+        
+    def has_collided(self, screen):
+        return
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -45,6 +81,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game.is_running = False
+            if event.type == pygame.KEYDOWN:
+                # Control
+                if event.key == pygame.K_LEFT:
+                    game.piece.horizontal_direction += -1
+                if event.key == pygame.K_RIGHT:
+                    game.piece.horizontal_direction += 1
+                if event.key == pygame.K_DOWN:
+                    game.piece.down(game.placed_pieces)
+                    
         game.display()
         pygame.display.update()
 
