49414846e347b15e2093e4899b80d0dcdd66d60b
Alice C <alice.chakroun123@gmail.com>
2024-02-02 10:34:11 +0100

    cdzf

--- piecepygame.py
+++ piecepygame.py
@@ -1,17 +1,35 @@
 class Piece:
     def __init__(self):
-        self.shape = np.array(random.choice(PIECES))
+        self.shape = np.array([1])
         self.position = [
             0,
             NUMBER_OF_TILES_WIDGHT // 2 - 1,
-        ]  # middle
-        self.colour = random.choice(PIECES_COLOURS)
+        ]  # middle pour l'instant, à modifier
+        self.colour = (135,206,235) # il est bleu !!!
 
     def display(self, screen):
-        for i in range(len(self.shape)):
-            for j in range(len(self.shape[i])):
-                if self.shape[i, j] == 1:
-                    x = (self.position[0] + i) * TILES_SIZE
-                    y = (self.position[1] + j) * TILES_SIZE
-                    rect = pygame.Rect(y, x, TILES_SIZE, TILES_SIZE)
-                    pygame.draw.rect(screen, self.colour, rect)
+        x = self.position[0] * TILES_SIZE
+        y = self.position[1] * TILES_SIZE
+        rect = pygame.Rect(y, x, TILES_SIZE, TILES_SIZE)
+        pygame.draw.rect(screen,self.colour,rect)
+
+    def new_position(self, screen):
+        former_position = np.copy(self.position)
+
+        if.... gauche:
+            self.shape = [former_position[0] -1 ,former_position[1]  ]
+        elif.... droite:
+            self.shape = [former_position[0] + 1 ,former_position[1] ]
+        elif.... haut:
+            self.shape = [former_position[0] ,former_position[1] +1]
+        elif.... droit :
+            self.shape = [former_position[0] ,former_position[1] -1 ]
+
+
+        if self.has_collided(screen) : #si il rentre dans un mur, # pour l'instant true/false mais lv up possible
+            self.shape = former_position
+        
+    def has_collided(self, screen):
+
+
+    
